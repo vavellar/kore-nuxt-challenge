dda44cd4a595a5932dd6c59caf697f3bb5443caa
Victor Avellar <victor.avellar@sonda.com>
2025-11-24 15:21:43 -0300

    feat: adding button to back if id doesnt match

--- app/pages/offering/[id].vue
+++ app/pages/offering/[id].vue
@@ -1,7 +1,13 @@
 <template>
-  <p v-if="error" class="text-red-500 text-center mt-10">
-    {{ error }}
-  </p>
+  <div v-if="error" class="w-fit mx-auto mt-20 flex flex-col items-center">
+    <p class="text-red-500 text-center mt-10">
+      {{ error }}
+    </p>
+    <NuxtLink to="/" class="mt-4">
+      Back
+    </NuxtLink>
+
+  </div>
   <div class="min-h-screen w-full border border-gray-100 max-w-[1920px] mx-auto" v-else>
     <Header />
     <div class="mt-20">
@@ -20,6 +26,7 @@ import Thumb from "../../../components/Thumb/Thumb.vue";
 import MainContent from "../../../components/MainContent/MainContent.vue";
 import FaqSection from "../../../components/MainContent/components/FaqSection.vue";
 import Footer from "../../../components/layout/Footer.vue";
+import BaseButton from "../../../components/BaseButton.vue";
 const route = useRoute()
 const { fetch, error } = useOffering()
 
