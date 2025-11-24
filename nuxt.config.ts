a4ed49beaf454ff0e0257b48d214f771f369d044
Victor Avellar <victor.avellar@sonda.com>
2025-11-24 16:57:49 -0300

    chore: configs to netlify deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['../assets/css/main.css'],
+  ssr: true,
+  nitro: {
+    preset: 'netlify'
+  },
   modules: [
     "@pinia/nuxt",
     '@nuxtjs/tailwindcss',
